079091432b8e931aaf55953336d64de63e895c9f
Luis Rodolfo Rodriguez Betancourt <lrbetancourt@uuci.cu>
2025-07-13 02:41:08 -0400

    Add: Basic protocols page

---
+++ src/pages/Protocols.jsx
@@ -0,0 +1,10 @@
+import ProtocolForm from '../features/protocols/ProtocolForm'
+
+export default function Protocols() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Protocolos</h1>
+      <ProtocolForm />
+    </div>
+  )
+}
