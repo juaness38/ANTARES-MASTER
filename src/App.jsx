742e9c72a14101386799d6c9ba42a05eb5dc675c
Luis <luiso.rb94@gmail.com>
2025-07-24 22:59:34 -0400

    feat(auth): remove old login flow in favor of Clerk integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import AppRouter from './routes/AppRouter'
-import { useEffect } from 'react'
-import useAuthStore from './store/authStore'
+import AppRouter from './routes/AppRouter';
 
 function App() {
-
-  const fetchUser = useAuthStore(state => state.fetchUser)
-
-  useEffect(() => {
-    fetchUser()
-  }, [])
-
   return (
-    
     <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-800 dark:text-white">
       <AppRouter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
